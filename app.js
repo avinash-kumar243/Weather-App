bc7d845af168a7c0c62d3df9bd014567de06bc81
Avinash kumar <avinashkumar40016@gmail.com>
2025-05-05 11:14:09 +0530

    Update2 app.js

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ const location_not_found = document.querySelector(`.location-not-found`);
 const weather_body = document.querySelector(`.weather-body`);
 
 async function checkWeather(city) {
-    const api_key = "ddac5c890ffcecbd912df5a74c0aa6c8";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-
-    const weather_data = await fetch(`${url}`).then(response => response.json());
-
     if(weather_data.cod === `404`) {
         location_not_found.style.display = "flex";
         weather_body.style.display = "none";
